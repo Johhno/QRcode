d51340732fc533d616b8104830a83eed1367cdde
Johhno <johntchen.mos@gmail.com>
2019-12-05 13:53:33 +1100

    sqlite

--- src/app/services/database.service.ts
+++ src/app/services/database.service.ts
@@ -1,9 +1,86 @@
+import { Platform } from '@ionic/angular';
 import { Injectable } from '@angular/core';
+import { SQLitePorter } from '@ionic-native/sqlite-porter/ngx';
+import { HttpClient } from '@angular/common/http';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { BehaviorSubject, Observable } from 'rxjs';
 
+
+export interface CapteurInterface{
+  id: number,
+  matricule: string,
+  emplacement: string
+}
 @Injectable({
   providedIn: 'root'
 })
 export class DatabaseService {
+  private database: SQLiteObject;
+  private dbReady: BehaviorSubject<boolean> = new BehaviorSubject(false);
 
-  constructor() { }
+  //Listener Connexion
+  capteursBS = new BehaviorSubject([]);
+
+  constructor(
+    private platform: Platform,
+    private sqlitePorter: SQLitePorter,
+    private sqlite: SQLite,
+    private http: HttpClient) { 
+
+      this.platform.ready().then(() => {
+        this.sqlite.create({
+          name: 'capteur.db',
+          location: 'default'
+        })
+        .then((db: SQLiteObject) => {
+            this.database = db;
+            this.seedDatabase();
+        });
+      });
+    }
+
+    seedDatabase() {
+      this.http.get('assets/seed.sql', { responseType: 'text'})
+      .subscribe(sql => {
+        this.sqlitePorter.importSqlToDb(this.database, sql)
+          .then(_ => {
+            this.loadCapteurs();
+            this.dbReady.next(true);
+          })
+          .catch(e => console.error(e));
+      });
+    }
+    getDatabaseState() {
+      return this.dbReady.asObservable();
+    }
+   
+    getDevs(): Observable<CapteurInterface[]> {
+      return this.capteursBS.asObservable();
+    }
+
+    loadCapteurs() {
+      return this.database.executeSql('SELECT * FROM capteur', []).then(data => {
+        let capteurs: CapteurInterface[] = [];
+   
+        if (data.rows.length > 0) {
+          for (var i = 0; i < data.rows.length; i++) {
+            capteurs.push({ 
+              id: data.rows.item(i).id,
+              matricule: data.rows.item(i).matricule, 
+              emplacement: data.rows.item(i).emplacement,
+             });
+          }
+        }
+        this.capteursBS.next(capteurs);
+      });
+    }
+
+    addCapteur(matricule, emplacement) {
+      let data = [matricule, emplacement];
+      return this.database.executeSql('INSERT INTO capteur (matricule, emplacement) VALUES (?, ?)', data).then(data => {
+        this.loadCapteurs();
+      });
+    }
+
+    
 }
--- src/assets/seed.sql
+++ src/assets/seed.sql
@@ -1,8 +1,9 @@
-CREATE TABLE IF NOT EXISTS developer(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,pass TEXT);
-INSERT or IGNORE INTO developer VALUES (1, 'Simon', '');
-INSERT or IGNORE INTO developer VALUES (2, 'Max', '');
-INSERT or IGNORE INTO developer VALUES (3, 'Ben', '');
+CREATE TABLE IF NOT EXISTS utilisateur(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT,pass TEXT);
+INSERT or IGNORE INTO utilisateur VALUES (1, 'T', 't');
+INSERT or IGNORE INTO utilisateur VALUES (2, 'Max', '');
+INSERT or IGNORE INTO utilisateur VALUES (3, 'Ben', '');
  
-CREATE TABLE IF NOT EXISTS product(id INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT, creatorId INTEGER);
-INSERT or IGNORE INTO product(id, name, creatorId) VALUES (1, 'Ionic Academy', 1);
-INSERT or IGNORE INTO product(id, name, creatorId) VALUES (2, 'Software Startup Manual', 1);
+CREATE TABLE IF NOT EXISTS capteur(id INTEGER PRIMARY KEY AUTOINCREMENT,matricule TEXT,emplacement TEXT);
+INSERT or IGNORE INTO capteur VALUES (1, 'capt1', '2');
+INSERT or IGNORE INTO capteur VALUES (2, 'capt2', '3');
+INSERT or IGNORE INTO capteur VALUES (3, 'capt3', '1');
