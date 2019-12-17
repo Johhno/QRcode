9ee556717f5109d3ba75c1ef455211f6b95feccf
Johhno <johntchen.mos@gmail.com>
2019-12-18 08:48:32 +1100

    sqlite

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'nav', pathMatch: 'full' },
+  { path: '', redirectTo: 'saisie', pathMatch: 'full' },
   { path: 'home', loadChildren: './home/home.module#HomePageModule'},
   { path: 'signin', loadChildren: './signin/signin.module#SigninPageModule' },
   { path: 'saisie', loadChildren: './saisie/saisie.module#SaisiePageModule' },
-  { path: 'nav', loadChildren: './nav/nav.module#NavPageModule' },
+
 
  
 
